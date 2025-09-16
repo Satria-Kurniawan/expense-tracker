8e4d5ebf82e885a7d035a3fba081272b30b67a6b
Satria-Kurniawan <satriakurniawan2001@gmail.com>
2025-09-16 10:58:12 +0800

    update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -42,6 +42,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        navigateFallback: "/index.html",
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
     }),
